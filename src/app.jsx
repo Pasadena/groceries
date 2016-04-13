383afc61bd915b6fe6d9231aef6761aff8b4a6a4
Simo-Pekka Koskinen <pasadenos@gmail.com>
2016-04-13 21:58:02 +0300

    More structuring

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ class App extends React.Component  {
     return (
       <div>
         <Header />
-        <div>Hello React world!?</div>
+        <Content />
         <Footer />
       </div>);
   }
@@ -15,6 +15,12 @@ class App extends React.Component  {
 class Header extends React.Component {
   render() {
     return <div className="header">This one is header</div>;
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    return <div className="container">Hello React world!?</div>;
   }
 }
 
