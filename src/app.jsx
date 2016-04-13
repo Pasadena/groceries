bb655f12569cfb2d47b17c8f3dbd6ad9d207a87d
Simo-Pekka Koskinen <pasadenos@gmail.com>
2016-04-13 21:51:29 +0300

    Improved app's initial structure

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,24 @@ import {render} from "react-dom"
 
 class App extends React.Component  {
   render() {
-    return <div>Hello React world!?</div>;
+    return (
+      <div>
+        <Header />
+        <div>Hello React world!?</div>
+        <Footer />
+      </div>);
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return <div className="header">This one is header</div>;
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return <div className="footer">And this one is footer</div>;
   }
 }
 
