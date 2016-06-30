25e89ad8da45307471f314b2fdab3966dce5d369
Simo-Pekka Koskinen <pasadenos@gmail.com>
2016-06-30 23:41:35 +0300

    Refactored action creators to more simple form

--- src/actions/GroceryActions.js
+++ src/actions/GroceryActions.js
@@ -1,51 +1,25 @@
-export const addGroceryItem = (item) => {
-  return {
-    type: "ADD_ITEM",
-    item
-  }
-}
+/**
+GroceryList item actions
+**/
+export const addGroceryItem = (item) => ({ type: "ADD_ITEM", item });
 
-export const toggleItemState = (id) => {
-  return {
-    type: "TOGGLE_ITEM_STATE",
-    id
-  }
-}
+export const deleteItem = (id) => ({ type: "DELETE_ITEM", id });
 
-export const changeItemName = (id, name) => {
-  return {
-    type: "CHANGE_ITEM_NAME",
-    id,
-    name
-  }
-}
+export const toggleItemState = (id) => ({ type: "TOGGLE_ITEM_STATE", id });
 
-export const changeItemAmount = (id, amount) => {
-  return {
-    type: "CHANGE_ITEM_AMOUNT",
-    id,
-    amount
-  }
-}
+export const changeItemName = (id, name) => ({ type: "CHANGE_ITEM_NAME", id, name });
 
-export const deleteItem = (id) => {
-  return {
-    type: "DELETE_ITEM",
-    id
-  }
-}
+export const changeItemAmount = (id, amount) => ({ type: "CHANGE_ITEM_AMOUNT", id, amount });
 
-export const closeList = (items) => {
-  return {
-    type: "CLOSE_LIST",
-    items
-  }
-}
+/**
+GroceryList actions
+**/
 
-export const groceryListsRequested = () => {
-  return {
-    type: "LISTS_REQUESTED"
-  }
-}
+export const closeList = (items) => ({ type: "CLOSE_LIST", items });
+
+/**
+Request status actions
+**/
+export const groceryListsRequested = () => ({ type: "LISTS_REQUESTED" });
 
 export const groceryListsLoaded = () => ({ type: "LISTS_LOADED" });
